b71291c8d8b7411e0d326571e4e2db6aa592a1f0
nonvegetable <100308933+nonvegetable@users.noreply.github.com>
2024-03-21 11:05:30 +0530

    Update render.js

--- src/render.js
+++ src/render.js
@@ -26,6 +26,9 @@ async function getVideoSources(){
     videoOptionsMenu.popup();
 }
 
+let mediaRecorder;
+const recordedChunks = [];
+
 async function selectSource(source){
     videoSelectionBtn.innerText = source.name;
 
@@ -44,4 +47,33 @@ async function selectSource(source){
 
     videoElement.srcObject = stream;
     videoElement.play();
+
+    const options = { mimeType: 'video/webm; codecs=vp9'};
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+}
+
+function handleDataAvailable(e){
+    console.log('video data available');
+    recordedChunks.push(e.data);
+}
+
+const { writeFile } = require('fs');
+
+async function handleStop(e){
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'Save video',
+        defaultPath: 'vid-${Date.now()}.webm'
+    });
+
+    console.log(filePath);
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
 }
